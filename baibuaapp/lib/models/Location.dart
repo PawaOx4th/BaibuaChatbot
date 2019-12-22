2b0ff7b8568505786519d39d8ba4a501a773d654
pawanachai <kanpim5@gmail.com>
2019-12-22 15:04:34 +0700

    + Add SpeedDia

---
+++ baibuaapp/lib/models/Location.dart
@@ -0,0 +1,8 @@
+
+class Location {
+  double latitude ;
+  double longtitude ;
+
+
+
+}
---
+++ baibuaapp/lib/models/Maplocationdata.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class MaplocationData {
+  final String nameLocation;
+
+  final double latitude;
+  final double longitude;
+
+  MaplocationData({this.nameLocation, this.latitude, this.longitude});
+}
+
+List maplocations = [
+  //0
+  MaplocationData(
+    nameLocation: "LocationEN",
+    latitude: 14.036339,
+    longitude: 100.726138,
+  ),
+  //1
+  MaplocationData(
+    nameLocation: "LocationBA",
+    latitude: 14.0368765,
+    longitude: 100.7307845,
+  ),
+  //2
+  MaplocationData(
+    nameLocation: "LocationHT",
+    latitude: 14.037252,
+    longitude: 100.728780,
+  ),
+  //3
+  MaplocationData(
+    nameLocation: "LocationFA",
+    latitude: 14.037249,
+    longitude: 100.727544,
+  ),
+  //4
+  MaplocationData(
+    nameLocation: "LocationAT",
+    latitude: 13.969284,
+    longitude: 100.6151239,
+  ),
+  //5
+  MaplocationData(
+    nameLocation: "LocationTE",
+    latitude: 14.0365526,
+    longitude: 100.799837,
+  ),
+  //6
+  MaplocationData(
+    nameLocation: "LocationAR",
+    latitude: 14.0379079,
+    longitude: 100.7297923,
+  ),
+  //7
+  MaplocationData(
+    nameLocation: "LocationST",
+    latitude: 14.039464,
+    longitude: 100.728787,
+  ),
+  //8
+  MaplocationData(
+    nameLocation: "LocationMT",
+    latitude: 14.0391852,
+    longitude: 100.7270293,
+  ),
+  //9
+  MaplocationData(
+    nameLocation: "LocationLA",
+    latitude: 14.038062,
+    longitude: 100.7282495,
+  ),
+  //10
+  MaplocationData(
+    nameLocation: "LocationTC",
+    latitude: 13.9692376,
+    longitude: 100.6156007,
+  ),
+  //11
+  MaplocationData(
+    nameLocation: "LocationNURSE",
+    latitude: 14.00363113,
+    longitude: 100.7285058,
+  ),
+];

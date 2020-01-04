50f7baaf624501f90838feea2d47545ad22fa8c5
Pawanachai <Kanpim5@gmail.com>
2020-01-04 15:06:57 +0700

    + new AppBar

--- baibuaapp/lib/screens/baibuaChatroom.dart
+++ baibuaapp/lib/screens/baibuaChatroom.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ChatroomBaibua extends StatefulWidget {
   ChatroomBaibua({Key key, this.title}) : super(key: key);
@@ -11,18 +12,124 @@ class ChatroomBaibua extends StatefulWidget {
   _HomePageDialogflowV2 createState() => new _HomePageDialogflowV2();
 }
 
-class _HomePageDialogflowV2 extends State<ChatroomBaibua>
-     {
-
-//  @override
-//  void dispose() {                                                              //********
-//    for (ChatMessage message in _messages)                                      // คืนค่า Memmory
-//      message.animationController.dispose();                                    //
-//    super.dispose();                                                            //********
-//  }
+class _HomePageDialogflowV2 extends State<ChatroomBaibua> {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
   bool _isComposing = false; // Check message null ?
+
+  //TextStyle
+  TextStyle _googleFontRoboto = GoogleFonts.roboto(
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold,
+    textStyle: TextStyle(color: Color.fromRGBO(0, 147, 233, 1)),
+  );
+
+//  Connect with Dialogflow => AuthGoogle => key.json file...
+  void Response(query) async {
+    _textController.clear();
+    AuthGoogle authGoogle =
+        await AuthGoogle(fileJson: "asset/key.json").build();
+    Dialogflow dialogflow =
+        Dialogflow(authGoogle: authGoogle, language: Language.thai);
+    AIResponse response = await dialogflow.detectIntent(query);
+    ChatMessage message = new ChatMessage(
+      text: response.getMessage() ??
+          new TypeMessage(response.getListMessage()[0]).platform,
+      name: "Baibua",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSubmitted(String text) {
+    String _textname = "ME";
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    ChatMessage message = new ChatMessage(
+      text: text,
+      name: _textname,
+      type: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    Response(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new AppBar(
+              centerTitle: true,
+              // Widget App Name
+              title: titleAppbar(),
+              backgroundColor: Colors.white,
+              // Widget Arrow back
+              leading: arrowBack(),
+              //Rounded Rectangle Border
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(25),
+                ),
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    width: 40,
+                    height: 40,
+                    child: IconButton(
+                      splashColor: Colors.lightBlueAccent,
+                      icon: Icon(
+                        Icons.home,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        //Call Page => Menu               
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: new Column(children: <Widget>[
+        Container(
+          child: new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+        ),
+        new Divider(height: 1.0),
+        new Container(
+          decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+          child: _buildTextComposer(),
+        ),
+      ]),
+    );
+  }
+
+//  ************************************ Widget Section ****************************** //
+//  Widget Component
   Widget _buildTextComposer() {
     return new IconTheme(
       data: new IconThemeData(color: Theme.of(context).accentColor),
@@ -59,210 +166,49 @@ class _HomePageDialogflowV2 extends State<ChatroomBaibua>
     );
   }
 
-//  Connect with Dialogflow => AuthGoogle => key.json file...
-  void Response(query) async {
-    _textController.clear();
-    AuthGoogle authGoogle =
-        await AuthGoogle(fileJson: "asset/key.json").build();
-    Dialogflow dialogflow =
-        Dialogflow(authGoogle: authGoogle, language: Language.thai);
-    AIResponse response = await dialogflow.detectIntent(query);
-    ChatMessage message = new ChatMessage(
-      text: response.getMessage() ??
-          new TypeMessage(response.getListMessage()[0]).platform,
-      name: "Baibua",
-      type: false,
+  // Widget App Name
+  Widget titleAppbar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Icon(
+          Icons.track_changes,
+          color: Colors.blue,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Text(
+          "Baibua",
+          style: _googleFontRoboto,
+        ),
+      ],
     );
-    setState(() {
-      _messages.insert(0, message);
-    });
   }
 
-  void _handleSubmitted(String text) {
-    String _textname = "ME";
-    _textController.clear();
-    setState(() {
-      _isComposing = false;
-    });
-    ChatMessage message = new ChatMessage(
-      text: text,
-      name: _textname,
-      type: true,
-
-    );
-    setState(() {
-      _messages.insert(0, message);
-    });
-    Response(text);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text("Baibuachatbot"),
-        leading: Icon(Icons.chat),
-        actions: <Widget>[
-          PopupMenuButton(
-            offset: Offset(0, 100),
-            elevation: 10,
-            color: Colors.blue,
-            //** (val) => value property in PopupMenuItem **//
-            onSelected: (val) {
-              Navigator.pushNamed(context, val);
-            },
-            icon: Icon(
-              Icons.keyboard_arrow_down,
-              size: 40,
-            ),
-            itemBuilder: (BuildContext context) {
-              return [
-                PopupMenuItem(
-                  //** value => 'routes/...' **//
-                  value: '/News-page',
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: Icon(
-                          Icons.fiber_new,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "ข่าวสาร",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                PopupMenuItem(
-                  //** value => 'routes/...' **//
-                  value: '/Setting-page',
-                  child: Row(
-                    children: <Widget>[
-                      Hero(
-                        tag: "setting",
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 10),
-                          child: Icon(
-                            Icons.settings,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        "ตั้งค่า",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                PopupMenuItem(
-                  //** value => 'routes/...' **//
-                  value: '/Map-page',
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: Icon(
-                          Icons.map,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "แผนที่",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                PopupMenuItem(
-                  //** value => 'routes/...' **//
-                  value: '/AddWork-page',
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: Icon(
-                          Icons.add_box,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "เพิ่มงาน",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                PopupMenuItem(
-                  //** value => 'routes/...' **//
-                  value: '/ViewWork-page',
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: Icon(
-                          Icons.work,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "ดูงาน",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ];
-            },
-          ),
-        ],
+  // Widget Arrow back
+  Widget arrowBack() {
+    return IconButton(
+      icon: Icon(
+        Icons.arrow_back_ios,
+        color: Colors.black,
       ),
-      body: new Column(children: <Widget>[
-        Container(
-          child: new Flexible(
-            child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
-            ),
-          ),
-        ),
-        new Divider(height: 1.0),
-        new Container(
-          decoration: new BoxDecoration(color: Theme.of(context).cardColor),
-          child: _buildTextComposer(),
-        ),
-      ]),
+      onPressed: () {
+        Navigator.pop(context);
+      },
     );
   }
+
+//  **************************************************************************//
 }
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text, this.name, this.type, });
+  ChatMessage({
+    this.text,
+    this.name,
+    this.type,
+  });
 
 //  AnimationController animationController; // Animation Control Chat UI
 
@@ -331,13 +277,12 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  new Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0),
-        child: new Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: this.type ? myMessage(context) : otherMessage(context),
-        ),
-      );
-
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
+    );
   }
 }
